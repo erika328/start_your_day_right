ce7ed6dafbd7d0182ae8f0b7617925969da80975
Erika Watanabe <erika03281996@yahoo.co.jp>
2024-06-08 16:58:04 +0900

    build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,12 +2,10 @@
 # exit on error
 set -o errexit
 
-# Bundle install and pristine bigdecimal gem
 bundle install
-bundle exec gem pristine psych --version 5.1.2
-
-# Precompile assets and migrate database
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+# bundle exec rake db:migrate
+
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
 bundle exec rake db:seed
